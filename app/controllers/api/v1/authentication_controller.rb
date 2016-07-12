cd2ca0c4793eb3334506b87f9c7c30a8dbdefb63
Norbert Forman <norbert@Norberts-MacBook-Pro.local>
2016-07-12 14:47:53 +0300

    login routes initial setup (wip)

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,13 @@
+module Api::V1
+  class AuthenticationController < BaseController
+
+    def sign_in
+      byebug
+    end
+
+    def sign_up
+      byebug
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :show] do
+        collection do
+          post :sign_in, to: "authentication#sign_in"
+          post :sign_up, to: "authentication#sign_up"
+        end
+      end
     end
   end
 end
