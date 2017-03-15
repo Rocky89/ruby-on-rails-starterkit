4f47b2ba35dc6189db52ca9073c90cbcdb6469e4
Csonka Erik <cerik@halcyonmobile.com>
2017-03-15 14:26:37 +0200

    Updated API documentations

--- spec/controllers/api/v1/authentication_controller_spec.rb
+++ spec/controllers/api/v1/authentication_controller_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Api::V1::AuthenticationController, type: :controller do
     DatabaseCleaner.clean
   end
   describe 'POST #sign_up' do
+    it 'Valid parameters - create user' do
+      post :sign_up, params: { email: 'tester@test.com', password: '12345678' }
+      expect(response).to have_http_status(200)
+      body = JSON.parse(response.body)
+      expect(body['user'].keys).to contain_exactly('id', 'email', 'auth_token', 'has_notifications')
+    end
     it 'Missing parameters' do
       post :sign_up
       expect(response).to have_http_status(400)
@@ -18,12 +24,6 @@ RSpec.describe Api::V1::AuthenticationController, type: :controller do
       post :sign_up, params: { email: 'tester@test.com' }
       expect(response).to have_http_status(400)
     end
-    it 'Valid parameters - create user' do
-      post :sign_up, params: { email: 'tester@test.com', password: '12345678' }
-      expect(response).to have_http_status(200)
-      body = JSON.parse(response.body)
-      expect(body['user'].keys).to contain_exactly('id', 'email', 'auth_token', 'has_notifications')
-    end
     it 'Valid parameters - duplicate email' do
       create(:user)
       post :sign_up, params: { email: 'test@test.com', password: '12345678' }
@@ -32,6 +32,13 @@ RSpec.describe Api::V1::AuthenticationController, type: :controller do
   end
 
   describe 'POST #sign_in' do
+    it 'Valid parameters - user sign in' do
+      create(:user)
+      post :sign_in, params: { email: 'test@test.com', password: '12345678' }
+      expect(response).to have_http_status(200)
+      body = JSON.parse(response.body)
+      expect(body['user'].keys).to contain_exactly('id', 'email', 'auth_token', 'has_notifications')
+    end
     it 'Missing parameters' do
       post :sign_in
       expect(response).to have_http_status(400)
@@ -43,13 +50,6 @@ RSpec.describe Api::V1::AuthenticationController, type: :controller do
     it 'Missing password parameter' do
       post :sign_in, params: { email: 'tester@test.com' }
       expect(response).to have_http_status(400)
-    end
-    it 'Valid parameters - user sign in' do
-      create(:user)
-      post :sign_in, params: { email: 'test@test.com', password: '12345678' }
-      expect(response).to have_http_status(200)
-      body = JSON.parse(response.body)
-      expect(body['user'].keys).to contain_exactly('id', 'email', 'auth_token', 'has_notifications')
     end
     it 'Valid parameters - invalid email' do
       create(:user)
@@ -63,6 +63,11 @@ RSpec.describe Api::V1::AuthenticationController, type: :controller do
     end
   end
   describe 'POST #forgot_password' do
+    it 'Valid parameters - valid email' do
+      create(:user)
+      post :forgot_password, params: { email: 'test@test.com' }
+      expect(response).to have_http_status(200)
+    end
     it 'Missing parameters' do
       post :forgot_password
       expect(response).to have_http_status(400)
@@ -71,11 +76,6 @@ RSpec.describe Api::V1::AuthenticationController, type: :controller do
       create(:user)
       post :forgot_password, params: { email: 'e@c.com' }
       expect(response).to have_http_status(404)
-    end
-    it 'Valid parameters - valid email' do
-      create(:user)
-      post :forgot_password, params: { email: 'test@test.com' }
-      expect(response).to have_http_status(200)
     end
   end
 end
